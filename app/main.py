e5361bc8630d553b821ecb323c4ae9066a992fcf
dominikmukrecki <dominik@mukrecki.pl>
2022-11-20 14:03:17 +0100

    commit

--- app/main.py
+++ app/main.py
@@ -1,13 +1,3 @@
 import uvicorn
 
-from fastapi import FastAPI
-
-app = FastAPI()
-
-from pydantic import BaseModel
-
-class QADataModel(BaseModel):
-    question: str
-    context: str
-
-import app.qna
+import app.endpoints
