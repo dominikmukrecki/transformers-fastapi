d17a9e5c67d962f75aa8307c9ccddaec196442d4
dominikmukrecki <dominik@mukrecki.pl>
2022-11-20 15:49:58 +0100

    qna imporve

--- app/main.py
+++ app/main.py
@@ -5,14 +5,14 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class QADataModel(BaseModel):
+class QNADataModel(BaseModel):
     question: str
     context: str
 
 from transformers import pipeline
-pipe = pipeline(model=os.environ['MODEL'])
+pipe = pipeline(model=os.environ['QNA_MODEL'])
 
-@app.post("/question_answering")
-async def qa(input_data: QADataModel):
+@app.post(os.environ['QNA_ENDPOINT'])
+async def qa(input_data: QNADataModel):
     result = pipe(question = input_data.question, context=input_data.context)
     return result
