970c21981a7f5af64e5af06dd5dfccd2f51c3937
dominikmukrecki <dominik@mukrecki.pl>
2022-11-20 14:04:37 +0100

    Update qna.py

--- app/endpoints/qna.py
+++ app/endpoints/qna.py
@@ -1,3 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+from pydantic import BaseModel
+
+class QADataModel(BaseModel):
+    question: str
+    context: str
+
 from transformers import pipeline
 model_name = 'henryk/bert-base-multilingual-cased-finetuned-polish-squad2'
 model = pipeline(model=model_name, tokenizer=model_name, task='question-answering')
