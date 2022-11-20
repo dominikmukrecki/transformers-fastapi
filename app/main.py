e0250acb76f7783817a391b298dbe31df8827b47
dominikmukrecki <dominik@mukrecki.pl>
2022-11-20 15:16:49 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ class QADataModel(BaseModel):
     question: str
     context: str
 
-from transformers import pipeline
+from transformers import pipeline, AutoModel, AutoTokenizer
 #model_name = 'azwierzc/herbert-large-poquad'
 model_name = AutoTokenizer.from_pretrained("/home/root/.cache/huggingface/transformers/herbert-large-poquad")
 model = pipeline(model=model_name, tokenizer=model_name, task='question-answering')
