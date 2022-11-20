2fcec5d7ae3cc06b1aa65a6cafe424c935fb4fdc
dominikmukrecki <dominik@mukrecki.pl>
2022-11-20 13:51:45 +0100

    commit

---
+++ app/qna.py
@@ -0,0 +1,8 @@
+from transformers import pipeline
+model_name = 'henryk/bert-base-multilingual-cased-finetuned-polish-squad2'
+model = pipeline(model=model_name, tokenizer=model_name, task='question-answering')
+
+@app.post("/question_answering")
+async def qa(input_data: QADataModel):
+    result = model(question = input_data.question, context=input_data.context)
+    return result
