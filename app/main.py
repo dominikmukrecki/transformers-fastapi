47e497a6d4b8d0d3517885481d3b8ba73b9009c3
dominikmukrecki <dominik@mukrecki.pl>
2022-11-20 15:10:56 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,9 +9,8 @@ class QADataModel(BaseModel):
     context: str
 
 from transformers import pipeline
-model_name = 'azwierzc/herbert-large-poquad'
+model_name = '/home/root/.cache/huggingface/transformers/herbert-large-poquad'
 model = pipeline(model=model_name, tokenizer=model_name, task='question-answering')
-model.save_pretrained("/home/root/.cache/huggingface/transformers")
 
 @app.post("/question_answering")
 async def qa(input_data: QADataModel):
