b424acfda926c81a90c5c620f9c3f24b48071623
dominikmukrecki <dominik@mukrecki.pl>
2022-11-23 23:13:41 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ class SentenceDataModel(BaseModel):
     corpus: list
 
 from sentence_transformers import SentenceTransformer, util
-model = SentenceTransformer(os.environ['SENTENCE_MODEL'])
+model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
 
 @app.post('/' + os.environ['SENTENCE_ENDPOINT'])
 async def sent(input_data: SentenceDataModel):
