e77fdc175630f87d920c0af5d20cdec01a1010b8
dominikmukrecki <dominik@mukrecki.pl>
2022-11-23 21:52:18 +0100

    test

--- app/main.py
+++ app/main.py
@@ -5,14 +5,26 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class QNADataModel(BaseModel):
-    question: str
-    context: str
+#class QNADataModel(BaseModel):
+#    question: str
+#    context: str
 
-from transformers import pipeline
-pipe = pipeline(model=os.environ['QNA_MODEL'])
+#from transformers import pipeline
+#pipe = pipeline(model=os.environ['QNA_MODEL'])
 
-@app.post('/' + os.environ['QNA_ENDPOINT'])
-async def qa(input_data: QNADataModel):
-    result = pipe(question = input_data.question, context=input_data.context)
+#@app.post('/' + os.environ['QNA_ENDPOINT'])
+#async def qa(input_data: QNADataModel):
+#    result = pipe(question = input_data.question, context=input_data.context)
+#    return result
+
+from sentence_transformers import SentenceTransformer
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+query_embedding = model.encode('How big is London')
+passage_embedding = model.encode(['London has 9,787,426 inhabitants at the 2011 census',
+                                  'London is known for its finacial district'])
+
+@app.post('/sentence')
+async def sentence():
+    result = util.dot_score(query_embedding, passage_embedding)
     return result
