1f00a18efafda859c6f749721c35453099a08954
dominikmukrecki <dominik@mukrecki.pl>
2022-11-23 23:31:08 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,18 +5,6 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-#class QNADataModel(BaseModel):
-#    question: str
-#    context: str
-
-#from transformers import pipeline
-#pipe = pipeline(model=os.environ['QNA_MODEL'])
-
-#@app.post('/' + os.environ['QNA_ENDPOINT'])
-#async def qa(input_data: QNADataModel):
-#    result = pipe(question = input_data.question, context=input_data.context)
-#    return result
-
 class SentenceDataModel(BaseModel):
     query: str
     corpus: list
