d86bffea813abb4d682b0479fb4d68755e264b2b
dominikmukrecki <dominik@mukrecki.pl>
2022-11-23 23:10:29 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -22,9 +22,9 @@ class SentenceDataModel(BaseModel):
     corpus: list
 
 from sentence_transformers import SentenceTransformer, util
-model = SentenceTransformer(os.environ['SENTENCE_MODEL'])
+model = SentenceTransformer(os.environ['QNA_MODEL'])
 
-@app.post('/' + os.environ['SENTENCE_ENDPOINT'])
+@app.post('/' + os.environ['SENTENCE_MODEL'])
 async def sent(input_data: SentenceDataModel):
     result = util.semantic_search(model.encode(query), model.encode(corpus), top_k=1)
     return result
