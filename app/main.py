8f5be4aeeda967cfb58ca604945c07417148e37f
dominikmukrecki <64379788+dominikmukrecki@users.noreply.github.com>
2022-11-25 11:09:08 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,21 +5,12 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-#class QNADataModel(BaseModel):
-#    question: str
-#    context: str
-
-#from transformers import pipeline
-#pipe = pipeline(model=os.environ['QNA_MODEL'])
-
-#@app.post('/' + os.environ['QNA_ENDPOINT'])
-#async def qa(input_data: QNADataModel):
-#    result = pipe(question = input_data.question, context=input_data.context)
-#    return result
-
 class SentenceDataModel(BaseModel):
     query: str
     corpus: list
+
+def myFunc(e):
+  return e['corpus_id']
 
 from sentence_transformers import SentenceTransformer, util
 model = SentenceTransformer(os.environ['SENTENCE_MODEL'])
@@ -27,4 +18,4 @@ model = SentenceTransformer(os.environ['SENTENCE_MODEL'])
 @app.post('/' + os.environ['SENTENCE_ENDPOINT'])
 async def sent(input_data: SentenceDataModel):
     result = util.semantic_search(model.encode(input_data.query), model.encode(input_data.corpus))
-    return result
+    return {'result': result.sort(key=myFunc)}
