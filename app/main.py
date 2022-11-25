e9ab012cbf6142c4b69157c0e188d7ea492b0330
dominikmukrecki <64379788+dominikmukrecki@users.noreply.github.com>
2022-11-25 11:20:55 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -15,4 +15,5 @@ model = SentenceTransformer(os.environ['SENTENCE_MODEL'])
 @app.post('/' + os.environ['SENTENCE_ENDPOINT'])
 async def sent(input_data: SentenceDataModel):
     result = util.semantic_search(model.encode(input_data.query), model.encode(input_data.corpus))
-    return {'result': result[0]}
+    def myFunc(e): return e['corpus_id']
+    return {'result': result[0].sort(key=myFunc)}
