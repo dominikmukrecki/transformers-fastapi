81a6b624b27e4f58ea2a26f01365144d392d5bfd
dominikmukrecki <64379788+dominikmukrecki@users.noreply.github.com>
2022-11-25 11:13:10 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,13 +9,10 @@ class SentenceDataModel(BaseModel):
     query: str
     corpus: list
 
-def myFunc(e):
-  return int(e['corpus_id'])
-
 from sentence_transformers import SentenceTransformer, util
 model = SentenceTransformer(os.environ['SENTENCE_MODEL'])
 
 @app.post('/' + os.environ['SENTENCE_ENDPOINT'])
 async def sent(input_data: SentenceDataModel):
     result = util.semantic_search(model.encode(input_data.query), model.encode(input_data.corpus))
-    return {'result': result.sort(key=myFunc)}
+    return {'result': result}
