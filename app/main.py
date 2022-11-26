28bae88e512ac50a4f68ed235c5fac976ec55d73
dominikmukrecki <dominik@mukrecki.pl>
2022-11-26 22:48:41 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,11 +8,12 @@ app = FastAPI()
 class SentenceDataModel(BaseModel):
     query: str
     corpus: list
+    top_k: int
 
 from sentence_transformers import SentenceTransformer, util
 model = SentenceTransformer(os.environ['SENTENCE_MODEL'])
 
 @app.post('/' + os.environ['SENTENCE_ENDPOINT'])
 async def sent(input_data: SentenceDataModel):
-    result = util.semantic_search(model.encode(input_data.query), model.encode(input_data.corpus), score_function=util.cos_sim, top_k=len(input_data.corpus))
+    result = util.semantic_search(model.encode(input_data.query), model.encode(input_data.corpus), score_function=util.cos_sim, top_k=input_data.top_k)
     return {'query': input_data.query, 'corpus': input_data.corpus, 'result': result, 'model': os.environ['SENTENCE_MODEL']}
