7cad986a68ab2e10b1b78e3a3d62c1f0d66c1a0d
dominikmukrecki <dominik@mukrecki.pl>
2022-11-26 23:26:12 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,9 +11,9 @@ class SentenceDataModel(BaseModel):
     top_k: int
 
 from sentence_transformers import SentenceTransformer, util
-model = SentenceTransformer(os.environ['SENTENCE_MODEL'])
+model = SentenceTransformer(os.environ['SENTENCE_MODEL']
 
 @app.post('/' + os.environ['SENTENCE_ENDPOINT'])
 async def sent(input_data: SentenceDataModel):
-    result = util.semantic_search(model.encode(input_data.query), model.encode(input_data.corpus), score_function=util.cos_sim, top_k=input_data.top_k)
+    result = util.semantic_search(model.encode(input_data.query), model.encode(input_data.corpus), score_function=util.dot_score, top_k=input_data.top_k)
     return {'query': input_data.query, 'corpus': input_data.corpus, 'result': result, 'model': os.environ['SENTENCE_MODEL']}
