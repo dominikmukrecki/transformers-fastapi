0a47c4b1289ff78f012cba24dedd5477caeac165
dominikmukrecki <dominik@mukrecki.pl>
2022-11-26 23:48:47 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -16,10 +16,11 @@ model = SentenceTransformer(os.environ['SENTENCE_MODEL'])
 
 @app.post('/' + os.environ['SENTENCE_ENDPOINT'])
 async def sent(input_data: SentenceDataModel):
-    if input_data.score_function == 'dot_score'
+    if input_data.score_function == 'dot_score':
         score_function = util.dot_score
-    elif input_data.score_function == 'cos_sim'
+    elif input_data.score_function == 'cos_sim':
         score_function = util.cos_sim
-    else return null
+    else:
+        return null
     result = util.semantic_search(model.encode(input_data.query), model.encode(input_data.corpus), score_function=util.dot_score, top_k=input_data.top_k)
     return {'query': input_data.query, 'corpus': input_data.corpus, 'result': result, 'model': os.environ['SENTENCE_MODEL']}
